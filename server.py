3d78741869cef3fa362dab552be4479726974fae
tingilee <tingilee@gmail.com>
2015-02-04 17:36:07 -0800

    added tokenizing headers and closing sockets

--- server.py
+++ server.py
@@ -9,17 +9,24 @@ import string
 import re
 from pprint import pprint
 
+SLEEP_TIME_BETWEEN_RECEIVING_DATA = 0.1
 TIMEOUT = 10 # seconds
+SOCKET_RECV_SIZE = 1024
 server_is_running = True
+port_listening = sys.argv[1]
 
-# Calls sys.exit
+# Exits the program
 def terminate():
-	# Exits the program
 	sys.exit()
 
+def message_log(message):
+	print time.strftime("%d %m %H:%M:%S") + " >>> " + message 
+
+# sets the isClosed condition to True
 def timeout_function(connection):
 	connection['isClosed'] = True
 
+# resets the timer for both tunnels 
 def reset_timer(connection):
 	#print "set_timer"
 	connection['timerLock'].acquire() 
@@ -30,46 +37,59 @@ def reset_timer(connection):
 	timer.start()
 	connection['timerLock'].release() 
 
+def closeSockets(clientsocket, hostsocket):
+	clientsocket.close()	
+	hostsocket.close()
+
+# a thread for tunnel from client -> proxy -> server
 def handle_forwarding_to_server(thisConnection):
 	print "new thread: handle_forwarding_to_server"
 	pprint(thisConnection)
+	# connect_tunneling == True
 	try:
 		while True:
-			#print "waiting for client requests"
-			time.sleep(0.1)
-			data = thisConnection['clientsocket'].recv(16) # size = ?
+			time.sleep(SLEEP_TIME_BETWEEN_RECEIVING_DATA)
+			data = thisConnection['clientsocket'].recv(SOCKET_RECV_SIZE) 
 			reset_timer(thisConnection)
 			print "Recv'd from client: " + data
 			if data:
 				#print "Sending to server: " + data
 				thisConnection['hostsocket'].sendall(data)
-			elif connect_tunneling:
-				if thisConnection['isClosed']:
-					break
+			elif thisConnection['isClosed']:			# half close? do we need an isClosed condition for each tunnel?
+				break
 			else:
 				break
 	except Exception as e:
 		pprint(e)
 	finally:
-		print "FINALLY end thread handle_forwarding_to_server clientsocket "
-		thisConnection['hostsocket'].close()
+		print "FINALLY end thread handle_forwarding_to_server"
+		closeSockets(thisConnection['clientsocket'], thisConnection['hostsocket'])
+		terminate()
 
 # ***** RENAME THIS CHUNK LATER ***** #
-
 def handle_client(clientsocket, address):
 	print "thread: handle_client"
+	connection_closed = False
+	connect_tunneling = False
+	host = ""
+	hostport = 80	# default
+
+	header_buffer = ""	# to be forwarded to server for non-CONNECT requests
 	header_array = []
 	previous_header_line = "temp"
 	current_header_line = ""
 
-	# TODO tokenize all the header lines
+	# Buffers up the header
 	while len(previous_header_line) != 0:
 		curr_byte = clientsocket.recv(1) # Read the next byte
-		#header_byte_buffer += curr_byte
 		if (curr_byte == '\n'):
-			# TODO: only add to header_array if its the type we ant
-			#print "header: " + current_header_line
-			header_array.append(current_header_line)
+			# tokenize the header lines
+			line_arr = re.split(' |\t', current_header_line)
+			line_buf = line_arr[0]
+			for i in range(1, len(line_arr)):
+				line_buf += " " + line_arr[i]
+			print "tokenized current header line: " + line_buf
+			header_array.append(line_buf)
 			previous_header_line = current_header_line;
 			current_header_line = ""
 			continue
@@ -77,56 +97,52 @@ def handle_client(clientsocket, address):
 			continue
 		current_header_line += curr_byte
 
-	connection_closed = False
-	host = ""
-	hostport = 80	# default
-	connect_tunneling = False
-
+	# Reads over the headers and make modifications to the headers
 	for i in range(0, len(header_array)):
 		line = header_array[i]
-		if i == 0:	# First line HTTP protocol
-			print  (time.strftime("%d %m %H:%M:%S")) + " >>> " + line # Do we print HTTP/1.0?
+		if i == 0:	
 			line_arr = re.split(' |\t',line)
 			# print line_arr
-			print line_arr[0].lower() 
 			line_arr[2] = "HTTP/1.0"
-			if (line_arr[0].lower() == "connect"):
+			if line_arr[0].upper() == "CONNECT":
 				print "CONNECT = TRUE"
 				connect_tunneling = True
-			if ("https://" in line_arr[1].lower()):				
-				hostport = 443
-
+			if "https://" in line_arr[1].lower():				
+				hostport = 443	
+			
 			header_array[0] = line_arr[0] + " " + line_arr[1] + " " + line_arr[2]
+			message_log(line_arr[0] + " " + line_arr[1])	# not log the HTTP/1.0
+
 		elif line[0:5].lower() == "host:":
 			host = line[6:].lower()
 			print "host: " + host
 			host_arr = host.rsplit(':', 1)		# split from the right, only split 1
 			# print host_arr
-			if (len(host_arr) > 1):
+			if len(host_arr) > 1:
 				try:
-				    value = int(host_arr[1])	# port number
+				    port_num = int(host_arr[1])
 				    host = host_arr[0]
-				    hostport = value
-				except ValueError:	# not an int(port)
+				    hostport = port_num
+				except ValueError:		# no port specified -> use default: 80 or 443
 				    pass 	
 
 		elif line.lower() == "connection: keep-alive":
 			connection_closed = True
-			line = "connection: close"
-			header_array[i] = line
+			header_array[i] = "connection: close"
 
 		elif line.lower() == "proxy-connection: keep-alive":
-			line = "proxy-connection: close"
-			header_array[i] = line
+			header_array[i]  = "proxy-connection: close"
+		header_buffer += header_array[i] + '\n'
 
+	# Opens a socket and connect to the server host
 	hostsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	host_address = (host, hostport)
 	print "Attempting connection to " + host + ":" + str(hostport)
 	connect_ret = hostsocket.connect_ex(host_address) 
 	if (connect_ret != 0):
-		print "host connect error: " + str(connect_ret)
+		print "host server connection error: " + str(connect_ret)
 
-	if (connect_tunneling):
+	if connect_tunneling:	# HTTP CONNECT
 		clientsocket.send('HTTP/1.0 200 OK\r\n\r\n')
 		thisConnection = {
 			"clientsocket" : clientsocket,
@@ -134,6 +150,7 @@ def handle_client(clientsocket, address):
 			"isClosed": False,
 			"timerLock" : threading.Lock()
 		}
+		# initialize and start timer
 		timer = threading.Timer(TIMEOUT, timeout_function, (thisConnection,))
 		timer.start()
 		thisConnection['timer'] = timer
@@ -141,17 +158,14 @@ def handle_client(clientsocket, address):
 		connect_handle_thread = threading.Thread(target=handle_forwarding_to_server, args=(thisConnection,))
 		connect_handle_thread.setDaemon(True)
 		connect_handle_thread.start()
-	else:
-		header_buffer = ""
-		for i in range(0, len(header_array)):
-			header_buffer += header_array[i] + '\n'
+	else:	# not HTTP CONNECT
 		print "header: " + header_buffer
 		hostsocket.sendall(header_buffer + "\r\n")
 
 	try:
 		while True:
-			time.sleep(0.1)
-			data = hostsocket.recv(1024) # size = ?
+			time.sleep(SLEEP_TIME_BETWEEN_RECEIVING_DATA)
+			data = hostsocket.recv(SOCKET_RECV_SIZE)
 			if connect_tunneling:
 				if thisConnection['isClosed']:
 					break
@@ -164,7 +178,8 @@ def handle_client(clientsocket, address):
 		pprint(e)
 	finally:
 		print "FINALLY end thread handle_client host: " + host + ": " + str(hostport)
-		#clientsocket.close()
+		closeSockets(clientsocket, hostsocket)
+		terminate()
 
 # ***** We create a separate thread to read for eof from console ***** #
 
@@ -191,20 +206,13 @@ user_input_thread.setDaemon(True)
 user_input_thread.start()
 
 # ***** Create the listening thread, which dispatches child threads for each new connection ***** #
-
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-server.bind((host, int(sys.argv[1])))
+server.bind((host, int(port_listening)))
 server.listen(5)
-print  (time.strftime("%d %m %H:%M:%S")) + " - Proxy listening on " + host + ":" + sys.argv[1]
+message_log(" - Proxy listening on " + host + ":" + port_listening)
 
-
-# Loops while server_is_running is true,
-# accepting packets from clients and 
-# sorting the packet into the proper Queue
-# for future processing. For more information see
-# handle_packet.
+# Loops while server_is_running is true, accepting packets from clients 
 def acceptConnections():
 	global server_is_running
 	while server_is_running:
@@ -215,15 +223,10 @@ def acceptConnections():
 		connection_handle_thread.setDaemon(True)
 		connection_handle_thread.start()
 
-
-
 # create thread which is accepting packets from clients
 server_connection_thread = threading.Thread(target=acceptConnections)
 server_connection_thread.setDaemon(True)
 server_connection_thread.start()
-
-
-
 
 # ***** Keep server alive until it is time ***** # 
 # Exit when server stops running
@@ -232,11 +235,5 @@ while server_is_running:
 	continue
 
 server.close()
-# End the process
 terminate()
-
-
-
-
-
 
